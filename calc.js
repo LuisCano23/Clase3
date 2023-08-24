47babb46da14bde18962d7b6647cc8e42db40bb5
luis <canito367@gmail.com>
2023-08-24 11:34:16 -0400

    menu de 4 opciones

--- calc.js
+++ calc.js
@@ -9,11 +9,27 @@ var n1,n2, suma;
 
 seguir=prompt("Bienvenido, quiere seguir?");
 if(seguir=="s") {
-    nombre = prompt("Ingrese nombre: ");
-    n1 = parseInt(prompt("Ingrese número 1: "));
-    n2 = parseFloat(prompt("Ingrese número 2: "));
-    suma = n1+n2;
-    console.log(suma);}
-
-else 
-    console.log("Adios");
+    opcion=parseInt(prompt("(1)Suma (2)Resta (3)Multiplicacion (4)Division"));
+    if (opcion==1){
+        nombre = prompt("Ingrese nombre: ");
+        n1 = parseInt(prompt("Ingrese número 1: "));
+        n2 = parseFloat(prompt("Ingrese número 2: "));
+        suma = n1+n2;
+        console.log("Esto es suma")
+        console.log(suma);
+    }
+    else if (opcion==2)
+    {
+        console.log("Resta")
+    }
+    else if(opcion==3)
+    {
+        console.log("Multiplicacion");
+    }
+    else {
+        console.log("Division")
+    }
+}
+else{
+    console.log("Chau")
+}      
